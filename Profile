610aeb1ca5f68d1e59e2eaed9a634a37cc955b78
mr-rakesh-ranjan <rakeshranjanhjp@gmail.com>
2022-09-27 13:31:09 +0530

    final wala done

--- Profile
+++ Profile
@@ -1,1 +1,1 @@
-web: gunicorn crud_django.wsgi --log-file-
+web: gunicorn crud_django.wsgi --log-file -
--- crud_django/settings.py
+++ crud_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qyu4n-fxf-z$7t%-lx7mnic$sil0s(w)v-st)d8@otwmlpcj4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
